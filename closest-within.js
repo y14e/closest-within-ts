1c7d670619e0730dd02fff95fc7f85635dc29452
Yusuke Kamiyamane <y@y14e.com>
2026-04-04 12:25:37 +0900

    20260404

--- closest-within.js
+++ closest-within.js
@@ -1,4 +1,4 @@
-export function closestWithin(selector, element, scope = document.documentElement) {
+export function closestWithin(element, selector, scope = document.documentElement) {
   if (!element || !scope) return null;
   for (let current = element; current && current !== scope; current = current.parentElement) {
     if (current.matches(selector)) return current;
--- closest-within.ts
+++ closest-within.ts
@@ -1,4 +1,4 @@
-export function closestWithin(selector: string, element: HTMLElement, scope = document.documentElement): HTMLElement | null {
+export function closestWithin(element: HTMLElement, selector: string, scope: HTMLElement = document.documentElement): HTMLElement | null {
   if (!element || !scope) return null;
   for (let current: HTMLElement | null = element; current && current !== scope; current = current.parentElement) {
     if (current.matches(selector)) return current;
