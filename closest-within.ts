001a2e294e3fa43a4db1f04a4071eddd5c50caec
Yusuke Kamiyamane <y@y14e.com>
2026-04-17 04:36:30 +0900

    Update closest-within.ts

--- closest-within.ts
+++ closest-within.ts
@@ -1,5 +1,7 @@
 export function closestWithin(element: Element, selector: string, scope: Element = document.documentElement): Element | null {
-  if (!element || !scope) return null;
+  if (!element || !scope) {
+    return null;
+  }
 
   if (element !== scope && !scope.contains(element)) {
     console.warn('Element not within scope.');
@@ -7,7 +9,9 @@ export function closestWithin(element: Element, selector: string, scope: Element
   }
 
   for (let current: Element | null = element; current && current !== scope; current = current.parentElement) {
-    if (current.matches(selector)) return current;
+    if (current.matches(selector)) {
+      return current;
+    }
   }
 
   return scope.matches(selector) ? scope : null;
