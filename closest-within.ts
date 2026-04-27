461dc0bd6b0134e86df05af4fcce449513826903
Yusuke Kamiyamane <y@y14e.com>
2026-04-27 11:47:31 +0900

    Update closest-within.ts

--- closest-within.ts
+++ closest-within.ts
@@ -1,4 +1,8 @@
-export function closestWithin(element: Element, selector: string, scope: Element = document.documentElement): Element | null {
+export function closestWithin(
+  element: Element,
+  selector: string,
+  scope: Element = document.documentElement,
+): Element | null {
   if (!element || !scope) {
     return null;
   }
