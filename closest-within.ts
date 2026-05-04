56b0ae5835e1ba4541ef438dcbb392f3734e0003
Yusuke Kamiyamane <y@y14e.com>
2026-05-04 14:29:15 +0900

    Update closest-within.ts

--- closest-within.ts
+++ closest-within.ts
@@ -1,7 +1,7 @@
 /**
  * closest-within.ts
  *
- * @version 1.0.0
+ * @version 1.0.1
  * @author Yusuke Kamiyamane
  * @license MIT
  * @copyright Copyright (c) 2026 Yusuke Kamiyamane
@@ -17,12 +17,19 @@ export function closestWithin(
   selector: string,
   scope: Element = document.documentElement,
 ): Element | null {
-  if (
-    !(element instanceof Element) ||
-    typeof selector !== 'string' ||
-    !(scope instanceof Element)
-  ) {
+  if (!(element instanceof Element)) {
+    console.warn('Invalid element');
     return null;
+  }
+
+  if (typeof selector !== 'string') {
+    console.warn('Invalid selector');
+    return null;
+  }
+
+  if (!(scope instanceof Element)) {
+    console.warn('Invalid scope element. Fallback: document.documentElement');
+    scope = document.documentElement;
   }
 
   if (element !== scope && !scope.contains(element)) {
